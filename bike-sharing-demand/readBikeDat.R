6d22ad187399c942978d7d3848a9f103c9e8cee4
Thomas Olsen <t.olsen18@outlook.com>
2023-09-13 13:04:51 -0600

    added data exploration

--- bike-sharing-demand/readBikeDat.R
+++ bike-sharing-demand/readBikeDat.R
@@ -2,6 +2,49 @@
 
 library(tidyverse)
 library(vroom)
-
+library(patchwork)
+library(DataExplorer)
 
 bikeData = vroom("train.csv")
+
+
+glimpse(bikeData)
+plot_bar(bikeData)
+
+# outliers/shape
+histograms = DataExplorer::plot_histogram(bikeData)
+
+# missing data
+intro_plot = plot_intro(bikeData)
+
+# collinearity
+corrPlot = plot_correlation(bikeData)
+
+# strong vs weak relationship
+humidityPlot = bikeData %>% ggplot(mapping = aes(x = humidity, y = count)) +
+  geom_point(color = "purple") +
+  geom_smooth(fill = "green") +
+  labs(
+    
+    title = "Count by humidity"
+    
+  ) + theme_bw()
+
+windPlot = bikeData %>% ggplot(mapping = aes(x = windspeed)) +
+  geom_histogram(fill = "black") +
+  labs(
+    
+    title = "Windspeed Histogram"
+    
+  ) + theme_bw()
+
+(humidityPlot+ corrPlot) / (windPlot + intro_plot)
+
+
+(humidityPlot+ corrPlot) / (histograms + intro_plot)
+
+
+
+
+
+
