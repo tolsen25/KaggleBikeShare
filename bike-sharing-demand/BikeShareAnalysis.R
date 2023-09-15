8366f4d66c0bc94b29a55289fb535d23b70c493c
Thomas Olsen <t.olsen18@outlook.com>
2023-09-15 13:10:27 -0600

    test

--- bike-sharing-demand/BikeShareAnalysis.R
+++ bike-sharing-demand/BikeShareAnalysis.R
@@ -18,7 +18,17 @@ View(bikeDataTrain)
 
 rFormula = count ~ datetime + season + holiday + workingday + weather + temp + atemp + humidity + windspeed
 
+my_recipe = recipe(rFormula, data = bikeDataTrain) %>%
+  step_corr(all_numeric_predictors(), threshold = 0.3) %>% 
+  step_time(datetime, features = c("hour")) %>% 
+  step_dummy(all_nominal_predictors()) %>%
+  step_rm(datetime) %>%
+  step_zv(all_predictors()) %>% 
+  step_num2factor(weather, levels = c("1","2","3", "4")) %>%
+  step_num2factor(season, levels = c("1","2","3"))
 
+preppedRecipe = prep(my_recipe)
 
+bakedDataTrain = bake(preppedRecipe, new_data = NULL) %>% select(count, everything())
 
-
+a = 2
