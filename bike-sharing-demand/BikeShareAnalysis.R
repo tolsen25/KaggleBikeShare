49b4af28dab478d680684904efd8cf27733e884f
Thomas Olsen <t.olsen18@outlook.com>
2023-09-18 13:08:46 -0600

    lin reg files

--- bike-sharing-demand/BikeShareAnalysis.R
+++ bike-sharing-demand/BikeShareAnalysis.R
@@ -24,11 +24,34 @@ my_recipe = recipe(rFormula, data = bikeDataTrain) %>%
   step_dummy(all_nominal_predictors()) %>%
   step_rm(datetime) %>%
   step_zv(all_predictors()) %>% 
-  step_num2factor(weather, levels = c("1","2","3", "4")) %>%
-  step_num2factor(season, levels = c("1","2","3"))
+  step_num2factor(weather, levels = c("1","2","3")) %>%
+  step_num2factor(season, levels = c("1","2","3", "4"))
 
 preppedRecipe = prep(my_recipe)
 
 bakedDataTrain = bake(preppedRecipe, new_data = NULL) %>% select(count, everything())
 
-a = 2
+# switching to tidymodels because will be easier to cross over in the future
+my_mod = linear_reg() %>%
+  set_engine("lm") # Engine = what R function to use
+
+bike_workflow = workflow() %>% add_recipe(preppedRecipe) %>%
+  add_model(my_mod) %>%
+  fit(data = bikeDataTrain)
+
+
+testData = vroom("test.csv")
+bikePreds = predict(bike_workflow, new_data = testData)
+
+sub = testData %>% mutate(
+  preds = ifelse(bikePreds$.pred < 0, 0, bikePreds$.pred),
+  datetime = as.character(format(datetime))
+                                   
+  ) %>% select(datetime, preds) 
+
+
+colnames(sub) = c("datetime", "count") 
+vroom_write(sub, "linRegSub1.csv", delim = ",")
+
+write.csv(sub, "screwVroom.csv")
+
