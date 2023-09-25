67138effa06b2fc0e878e15ec7e7f2abfe42ab19
Thomas Olsen <t.olsen18@outlook.com>
2023-09-25 13:53:30 -0600

    penalized regs

--- bike-sharing-demand/BikeShareAnalysis.R
+++ bike-sharing-demand/BikeShareAnalysis.R
@@ -118,7 +118,7 @@ rFormula = count ~ datetime + season + holiday + workingday + weather + temp + a
 my_recipe = recipe(rFormula, data = logTrain) %>%
   step_corr(all_numeric_predictors(), threshold = 0.3) %>% 
   step_mutate(weather = ifelse(weather  >3, 3, weather)) %>%
-  step_mutate(count = log(count)) %>% # log of the count
+  #step_mutate(count = log(count)) %>% # log of the count
   step_time(datetime, features = c("hour")) %>% 
   step_rm(datetime) %>%
   step_zv(all_predictors()) %>% 
@@ -128,14 +128,37 @@ my_recipe = recipe(rFormula, data = logTrain) %>%
   step_normalize(all_numeric_predictors())
 
 preppedRecipe = prep(my_recipe)
+#bake(preppedRecipe, logTrain)
 
-preg_model = linear_reg(penalty = 0, mixture = 0) %>%
+preg_model = linear_reg(penalty = tune(), mixture = tune()) %>%
   set_engine("glmnet")
 
 preg_wf = workflow() %>% add_recipe(my_recipe) %>%
   add_model(preg_model) %>% fit(data = logTrain)
+# what is the difference between L,v/k, and repeats
 
-logSub = predict(preg_wf, new_data = testData)
+tuning_grid = grid_regular(penalty(), mixture(), levels = 5)
+
+folds = vfold_cv(logTrain, v = 10, repeats = 1)
+
+CV_results = preg_wf %>% tune_grid(resamples = folds, grid = tuning_grid,
+                                   metrics = metric_set(rmse,mae,rsq))
+
+collect_metrics(CV_results) %>%
+  filter(.metric == "rmse") %>%
+  ggplot(data=., aes(x=penalty, y = mean, color =factor(mixture)))+
+  geom_line()
+
+bestTune = CV_results %>% select_best("rmse")
+
+final_wf = preg_wf %>% finalize_workflow(bestTune) %>%
+  fit(data = logTrain)
+
+logSub = final_wf %>% predict(new_data = testData)
+
+
+
+#logSub = predict(preg_wf, new_data = testData)
 #     mutate(.pred = exp(.pred)))
 
 sub = testData %>% mutate(
@@ -144,10 +167,60 @@ sub = testData %>% mutate(
   
 ) %>% select(datetime, count) 
 
-vroom_write(sub, "penRegSub1.csv", delim = ",")
+vroom_write(sub, "tunedPenalty.csv", delim = ",")
 
 #bakedDataTrain = bake(preppedRecipe, new_data = NULL) %>% select(count, everything())
 
+bikeDataTrain = vroom("train.csv")
+# Tuning Penalized Regression ---------------------------------------------
+
+logTrain = bikeDataTrain %>%
+  mutate(count = log(count))
+# bikeDataTrain = bikeDataTrain  %>% mutate(
+#   
+#   weather = ifelse(weather  >3, 3, weather)
+#   
+# ) %>% select(-c(casual, registered))
+# 
+# #View(bikeDataTrain)
+
+rFormula = count ~ datetime + season + holiday + workingday + weather + temp + atemp + humidity + windspeed
+
+my_recipe = recipe(rFormula, data = logTrain) %>%
+  step_corr(all_numeric_predictors(), threshold = 0.3) %>% 
+  step_mutate(weather = ifelse(weather  >3, 3, weather)) %>%
+  step_mutate(count = log(count)) %>% # log of the count
+  step_time(datetime, features = c("hour")) %>% 
+  step_rm(datetime) %>%
+  step_zv(all_predictors()) %>% 
+  step_num2factor(weather, levels = c("1","2","3")) %>%
+  step_num2factor(season, levels = c("1","2","3", "4")) %>%
+  step_dummy(all_nominal_predictors()) %>%
+  step_normalize(all_numeric_predictors())
+
+
+
+preppedRecipe = prep(my_recipe)
+
+preg_model = linear_reg(penalty = 0, mixture = 0) %>%
+  set_engine("glmnet")
+
+preg_wf = workflow() %>% add_recipe(my_recipe) %>%
+  add_model(preg_model) %>% fit(data = logTrain)
+
+
+
+
+
+# formatting stuff --------------------------------------------------------
+
+sub = testData %>% mutate(
+  count = exp(logSub$.pred),
+  datetime = as.character(format(datetime))
+  
+) %>% select(datetime, count) 
+
+vroom_write(sub, "penRegSub1.csv", delim = ",")
 
 
 
